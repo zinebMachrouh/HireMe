996ec33a2bd5b4ce94803dc76e82131b194fb379
zinebMachrouh <zinebmac49@gmail.com>
2024-01-31 09:24:47 +0100

    seeding categories

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 1; $i <= 15; $i++) {
+            Category::create([
+                'name' => "Category $i",
+            ]);
+        }
+    }
+}
